41edea25e424f43392718d7e637221abf5638891
Taha Ansari <taha.m.ansari@gmail.com>
2023-02-09 16:50:54 +0530

    wip

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
+
+const CakeContainer = (props) => {
+  return (
+    <div>
+      <h2>Number of Cake {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => {
+      dispatch(buyCake());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
