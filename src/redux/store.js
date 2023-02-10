b51d8df3d54f26fc6bed36e95161b7c382a0d990
Taha Ansari <taha.m.ansari@gmail.com>
2023-02-10 16:07:25 +0530

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 export default store;
